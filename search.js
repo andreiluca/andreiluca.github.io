140a5903ecefcebf00a6e3966ef2136ec9398b0e
Andrei Luca <andrei@andreiluca.com>
2015-06-29 13:48:22 +0300

    Search

--- search.js
+++ search.js
@@ -15,3 +15,24 @@ var idx = lunr(function () {
 for(var index in docs) {
   idx.add(docs[index]);
 }
+
+$(function() {
+  $("#search button").click(function() {
+    search();
+  });
+  $("#search input").keypress(function(e) {
+    if(e.which == 13) {
+      e.preventDefault();
+      search();
+    }
+  });
+})
+
+function search() {
+  var result = idx.search($("#search input").val());
+  if(result && result.length > 0) {
+    window.location.replace(result[0].ref);
+  } else {
+    alert("Found nothing");
+  }
+}
